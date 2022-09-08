57e08f6e71b2f70b9f1cccbb586aaa7e57be3dea
小吉君 <owocc@qq.com>
2022-09-08 18:51:20 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://192.168.31.73:80', // 代理目标的基础路径
+        target: 'https://47.101.63.12/exam_api', // 代理目标的基础路径
         changeOrigin: true,
         pathRewrite: { '^/api': '' }
       }
